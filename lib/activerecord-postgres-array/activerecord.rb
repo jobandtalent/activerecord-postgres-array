016a1f4f3a9810542e6f58a7227ae8960d0981bd
Fernando Tapia Rico <fertapric@gmail.com>
2017-10-02 15:36:53 +0200

    Fix quotes in array serialization

--- lib/activerecord-postgres-array/activerecord.rb
+++ lib/activerecord-postgres-array/activerecord.rb
@@ -15,8 +15,7 @@ module ActiveRecord
           if include_readonly_attributes || !self.class.readonly_attributes.include?(name)
             value = read_attribute(name)
             if column.type.to_s =~ /_array$/ && value && value.is_a?(Array)
-              prepared_statements = connection.instance_variable_get(:@config).fetch(:prepared_statements) { true }
-              value = "'#{value.to_postgres_array(new_record? && prepared_statements)}'"
+              value = value.to_postgres_array
             elsif klass.serialized_attributes.include?(name)
               value = @attributes[name].serialized_value
             end
